0d3cc3c7fa3ee3d3b548e93934a623ba653c8108
brandon <brandon@behemoth.ca>
2015-06-12 16:07:58 -0700

    Integrated with Slack webhooks, added cron

--- scrape.py
+++ scrape.py
@@ -1,79 +1,157 @@
 import requests, json, sys, re, moment
 from bs4 import BeautifulSoup
 
+dev = True  
+
+gameTitleChannelMap = {
+    "Crota's End - Normal Mode": "crota-nm",
+    "Crota's End - Hard Mode": "crota-hm",
+    "Vault of Glass - Normal Mode": "vog-nm",
+    "Vault of Glass - Hard Mode": "vog-hm",
+    "Prison of Elders": "prison-of-elders",
+    "Trials of Osiris": "trials-of-osiris",
+    "Weekly Nightfall Strike": "nightfall",
+    "Weekly Heroic Strike": "weekly-heroic",
+    "Strike Playlist": "strike-playlist",
+    "The Shadow Thief": "strike-playlist",
+    "The Devil's Lair": "strike-playlist",
+    "The Summoning Pits": "strike-playlist",
+    "The Nexus": "strike-playlist",
+    "Winter's Run": "strike-playlist",
+    "Cerberus Vae III": "strike-playlist",
+    "Dust Palace": "strike-playlist",
+    "The Undying Mind": "strike-playlist",
+    "The Will of Crota": "strike-playlist",
+    "Story Mission": "up-for-anything",
+    "Daily Heroic Story": "daily-heroic",
+    "Bounty": "up-for-anything",
+    "Patrol": "patrol",
+    "Iron Banner": "iron-banner",
+    "Crucible": "crucible",
+    "Exotic Weapon Bounty": "up-for-anything",
+    "Queen's Kill Order": "up-for-anything",
+    "Miscellaneous": "up-for-anything"
+}
+
+keywordChannelMap = {
+    "gorgons? (chest|giveaway)": "gorgon",
+    "drunk[- ]raid": "drunk-raids",
+    "fatebringer": "fatebringerless"
+}
+
+### Game class
+
 class Game:
-    def __init__(self):
-        self.time = ""
-        self.status = ""
-        self.url = ""
-        self.name = ""
-        self.requiredLevel = ""
-        self.description = ""
-        self.platform = ""
-        self.isMicRequired = True
-        self.players = []
+    def __init__(self, o):
+        self.gameDetails = self.parseGameDetails(o)
+        self.gameText = self.parseGameText(o)
+        self.time = self.parseTime(o)
+        self.status = self.parseStatus(o)
+        self.url = self.parseUrl(o)
+        self.title = self.parseTitle(o)
+        self.requiredLevel = self.parseRequiredLevel(o)
+        self.description = self.parseDescription(o)
+        self.platform = self.parsePlatform(o)
+        self.isMicRequired = self.parseMicRequired(o)
+        self.players = self.parsePlayers(o)
+        self.host = self.parseHost(o)
+        self.partySize = self.parsePartySize(o)
+        self.maxPlayers = self.parseMaxPlayers(o)
+        self.gameId = self.parseGameId(o)
+        self.channels = self.parseChannels(o)
 
-    def addPlayer(self, x):
-        self.players.append(x)
+    ### class utilities
 
-def parseTime(o):
-    mT = re.search(r"(\d\d):(\d\d) ([A-Z]{2})", o.find("h4", { "class": "issue-item-text"}).text)
-    time = { "hour": mT.group(1), "minute": mT.group(2) }
-    if (mT.group(3) == "PM"):
-        time['hour'] = str(int(time['hour']) + 12)
-    mD = re.search(r"\w{3}, (\d\d-\d\d) [A-Z]{3}", o.find("small").text)
-    date = str(moment.now().year) + "-" + str(mD.group(1)) + "T" + time['hour'] + ":" + time['minute'] + ":00"
-    return date
+    def addPlayer(self, player):
+        self.players.append(player)
 
-def parseStatus(o):
-    status = o.find("a", { "class": "btn-block"})
-    if (status):
-        return status.text
-    else:
-        return "Over"
+    ### internal functions
 
-def parseUrl(o):
-    url = o.find("div", { "class": "project-item-button"}).find('a', text=re.compile(r"View Lobby"), href=True)['href']
-    url = "https://www.the100.io" + url
-    return url
+    def parseGameDetails(self, o):
+        return ' '.join(o.find("span", { "class": "badge"}).parent.text.replace('\n', ' ').replace('\r', '').split())
 
-def parseName(o):
-    name = o.find("h4", { "class": "issue-item-text"}).text
-    name = re.search(r"\|(.*)", name).group(1).rstrip().lstrip()
-    return name
+    def parseGameText(self, o):
+        return o.find("h4", { "class": "issue-item-text"}).text.lstrip().rstrip()
 
-def parseRequiredLevel(o):
-    lvl = o.find("span", { "class": "badge"}).parent.text
-    lvl = re.search(r"lvl (\d+)", lvl, re.M)
-    if (lvl): return int(lvl.group(1))
-    return None
+    ### parsing functions
 
-def parseDescription(o):
-    description = o.find("h4", { "class": "issue-item-text"}).findNext('p').text.rstrip().lstrip()
-    return description
+    def parseGameId(self, o):
+        url = self.url if hasattr(self, 'url') else self.parseUrl(o)
+        return int(url.rsplit("/")[-1])
 
-def parsePlatform(o):
-    badge = o.find("span", { "class": "badge"}).text
-    return badge
+    def parseTime(self, o):
+        mT = re.search(r"(\d\d):(\d\d) ([A-Z]{2})", self.gameText)
+        time = { "hour": mT.group(1), "minute": mT.group(2) }
+        if (mT.group(3) == "PM"):
+            time['hour'] = str(int(time['hour']) + 12)
+        mD = re.search(r"\w{3}, (\d\d-\d\d) [A-Z]{3}", o.find("small").text)
+        date = str(moment.now().year) + "-" + str(mD.group(1)) + "T" + time['hour'] + ":" + time['minute'] + ":00" + moment.now().locale("US/Pacific").strftime('%z')
+        return date
 
-def parseMicRequired(o):
-    mic = o.find("span", { "class": "badge"}).parent.text
-    mic = re.search(r"mic required", mic, re.M)
-    if (mic):
-        return True
-    else:
-        return False
+    def parseStatus(self, o):
+        status = o.find("a", { "class": "btn-block"})
+        return status.text if (status) else "Over"
 
-def parsePlayers(o):
-    players = []
-    for player in o.find("h4", { "class": "issue-item-text"}).parent.findAll('a', href=True):
-        players.append({"name": player.text, "url": "https://www.the100.io" + player['href']})
-    return players
+    def parseUrl(self, o):
+        url = o.find("div", { "class": "project-item-button"}).find('a', text=re.compile(r"View Lobby"), href=True)['href']
+        url = "https://www.the100.io" + url
+        return url
 
-###
+    def parseTitle(self, o):
+        title = re.search(r"\|(.*)", self.gameText).group(1).rstrip().lstrip()
+        return title
+
+    def parseRequiredLevel(self, o):
+        lvl = re.search(r"lvl (\d+)", self.gameDetails)
+        return int(lvl.group(1)) if (lvl) else None
+
+    def parseDescription(self, o):
+        return o.find("h4", { "class": "issue-item-text"}).findNext('p').text.rstrip().lstrip()
+
+    def parsePlatform(self, o):
+        return o.find("span", { "class": "badge"}).text
+
+    def parseMicRequired(self, o):
+        mic = re.search(r"mic required", self.gameDetails)
+        return True if (mic) else False
+
+    def parsePlayers(self, o):
+        players = []
+        for player in o.find("h4", { "class": "issue-item-text"}).parent.findAll('a', href=True):
+            players.append({"name": player.text, "url": "https://www.the100.io" + player['href']})
+        return players
+
+    def parseHost(self, o):
+        return self.players[0] if (len(self.players) > 0) else parsePlayers(o)[0]
+
+    def parsePartySize(self, o):
+        partySize = re.search(r"(\d+) players? / \d+", self.gameDetails)
+        return int(partySize.group(1)) if (partySize) else 0
+
+    def parseMaxPlayers(self, o):
+        maxPlayers = re.search(r"\d+ players? / (\d+)", self.gameDetails)
+        return int(maxPlayers.group(1)) if (maxPlayers) else 0
+
+    def parseChannels(self, o):
+        channels = []
+        for key, value in gameTitleChannelMap.iteritems():
+            m = re.search(key, self.title, re.I)
+            if (m):
+                channels.append(value)
+                break
+
+        for key, value in keywordChannelMap.iteritems():
+            m = re.search(key, self.description, re.I)
+            if (m):
+                channels.append(value)
+        return channels
+
+### main
 
 if len(sys.argv) > 1:
     group = sys.argv[1]
+elif (dev == True):
+    group = "186"
 else:
     print "You must pass a group ID from the100.io as an argument to this script."
     sys.exit(1)
@@ -87,16 +165,9 @@ api = {}
 api['games'] = []
 
 for o in soup.findAll("div", { "class": "issue-item" }):
-    game = Game()
-    game.time = parseTime(o)
-    game.status = parseStatus(o)
-    game.url = parseUrl(o)
-    game.name = parseName(o)
-    game.requiredLevel = parseRequiredLevel(o)
-    game.description = parseDescription(o)
-    game.platform = parsePlatform(o)
-    game.isMicRequired = parseMicRequired(o)
-    game.players = parsePlayers(o)
-    api['games'].append(game.__dict__)
+    game = Game(o).__dict__
+    game.pop("gameDetails", None)
+    game.pop("gameText", None)
+    api['games'].append(game)
 
 print json.dumps(api, sort_keys=True, indent=4)
--- server.js
+++ server.js
@@ -4,31 +4,162 @@ var util = require('util');
 var async = require('async');
 var apigee = require('apigee-access');
 var cache = apigee.getCache('the100');
+var moment = require('moment');
 var app = express();
 var CronJob = require('cron').CronJob;
 
+var baasUrl = "https://api.usergrid.com/the100/slack";
+var slackWebHookUrl = "https://hooks.slack.com/services/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";
+var defaultGroup = "186";
+var token = "";
+var quotes = [] // Not used
+
 app.get('/', function(req, res) {
     var fullUrl = util.format("%s://%s%s", req.protocol, req.get('host'), req.originalUrl);
-    res.json({hint: util.format("Append a url segment with a group ID from the100.io, e.g. %s100", fullUrl)})
+    res.json({
+        hint: util.format("Append a url segment with a group ID from the100.io, e.g. %s100", fullUrl)
+    })
 });
 
 app.get('/:group', function(req, res) {
-    var python = require('child_process').spawn(
-        'python',
-        // second argument is array of parameters, e.g.:
-        ["scrape.py", req.params.group]
-    );
-    var output = "";
-    python.stdout.on('data', function(data) {
-        output += data
-    });
-    python.on('close', function(code) {
-        if (code !== 0) {
-            return res.status(500).json({error: "Error loading web data."})
+    get100Data(req.params.group, function(err, results) {
+        if (err) {
+            res.status(500).json({
+                error: "Error loading web data."
+            });
+        } else {
+            res.json(results.scrape);
+            token = results.token.access_token;
+            scrapeHandler(results.scrape, function(success) {
+                if (success) {
+                    console.log("Job completed successfully");
+                }
+            });
         }
-        output = JSON.parse(output)
-        return res.json(output)
     });
 });
 
 app.listen(process.env.PORT || 8000);
+
+var job = new CronJob('0 * * * * *', function() {
+    console.log("Started cron job");
+    get100Data(defaultGroup, function(err, results) {
+        token = results.token.access_token;
+        scrapeHandler(results.scrape, function(success) {
+            if (success) {
+                console.log("Job completed successfully");
+            }
+        });
+    });
+}, function() {
+    console.log("Cron job finished");
+}, true, null);
+
+function get100Data(group, callback) {
+    async.parallel({
+            token: function(callback) {
+                request.post({
+                        url: baasUrl + "/token",
+                        json: true,
+                        body: {
+                            "client_id": "YXA6_KYXsBEfEeWDGf9DpoauPA",
+                            "client_secret": "YXA6G6EJpalNy2-48qgBkoSo8O_wzgE",
+                            "grant_type": "client_credentials",
+                            "ttl": 0
+                        }
+                    },
+                    function(e, r, body) {
+                        callback(null, body);
+                    });
+            },
+            scrape: function(callback) {
+                var python = require('child_process').spawn(
+                    'python', ["scrape.py", group]
+                );
+                var scrapedData = "";
+                python.stdout.on('data', function(data) {
+                    scrapedData += data
+                });
+                python.on('close', function(code) {
+                    if (code !== 0) {
+                        callback(true);
+                    } else {
+                        var games = JSON.parse(scrapedData).games
+                        callback(null, games);
+                    }
+                });
+            }
+        },
+        function(err, results) {
+            callback(err, results);
+        });
+}
+
+function scrapeHandler(games, callback) {
+    async.each(games, function(game, callback) {
+            request.get({
+                url: baasUrl + "/games?ql=where%20gameId=" + game.gameId,
+                auth: {
+                    bearer: token
+                },
+                json: true
+            }, function(e, r, body) {
+                if (body.count === 0) {
+                    console.log(util.format("Creating new game for %s", game.gameId))
+                        request.post({
+                            url: baasUrl + "/games",
+                            auth: {
+                                bearer: token
+                            },
+                            json: true,
+                            body: game
+                        });
+                    notify(game);
+                } else {
+                    console.log(util.format("Updating %s (%s)", game.gameId, body.entities[0].uuid))
+                    request.put({
+                        url: util.format("%s/games/%s", baasUrl, body.entities[0].uuid),
+                        auth: {
+                            bearer: token
+                        },
+                        json: true,
+                        body: game
+                    });
+                }
+            })
+        },
+        function(err) {
+            callback(!err);
+        });
+}
+
+function notify(game) {
+    var relativeTime = (moment(game.time).diff(Date.now(), 'minutes') > 0) ? moment(game.time).fromNow() : moment(game.time).toNow();
+    var utcTime = util.format("%s GMT", moment(game.time).utc().format("MMM D, hh:mma"));
+    var availableSpots = (game.maxPlayers - game.partySize) >= 0 ? game.maxPlayers - game.partySize : 0;
+    if (availableSpots > 0) {
+        async.each(game.channels, function(channel, callback) {
+            request.post({
+                url: slackWebHookUrl,
+                json: true,
+                body: {
+                    "attachments": [{
+                        "color": "#ddd",
+                        "fallback": game.description,
+                        "text": game.description
+                    }],
+                    "channel": channel,
+                    "icon_url": "https://www.the100.io/apple-touch-icon.png",
+                    "text": util.format("New game by <@%s|%s> — *<%s|%s>*\nStarting *~%s* (%s) — need *%s level %s+* guardians", game.host.name, game.host.name, game.url, game.title, relativeTime, utcTime, availableSpots, game.requiredLevel),
+                    "username": "the100"
+                }
+            }, function(e, r, body) {
+                if (body == "ok") {
+                    console.log(util.format("Notification posted to %s for game %s", channel, game.gameId));
+                } else {
+                    console.log(util.format("Error sending Slack notification: %s", body))
+                }
+            });
+        });
+    }
+}
